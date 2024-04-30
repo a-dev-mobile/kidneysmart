9abfcfcc1ee9eb3403ff8dd2505b5d560dd8e979
Трофимов Дмитрий <trofimovdi@mts.ru>
2024-04-30 23:13:18 +0300

    wip

--- mobile/kidneysmart_app/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ mobile/kidneysmart_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\DEV\MY\MY_GITHUB\kidneysmart"
+export "FLUTTER_APPLICATION_PATH=C:\DEV\MY\MY_GITHUB\kidneysmart\mobile\kidneysmart_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
